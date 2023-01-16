720ffc9b966df0cc19bad634aec70815896cbe8f
fito_ <fito_@Fito>
2023-01-16 15:37:42 -0300

    comienzo

---
+++ GitHab/src/ProbamosGitHub.java
@@ -0,0 +1,17 @@
+
+public class ProbamosGitHub {
+
+	public static void main(String[] args) {
+
+		suma(2, 3);
+		
+	}
+public static int suma(int a, int b) {
+ int rata = a+b;
+ 
+ System.out.println(rata);
+	return rata;
+	// TODO Auto-generated method stub
+
+}
+}
