99cc2203e90ca6ee4b1670d60f158443ef2b1a5c
fito_ <fito_@Fito>
2023-01-16 15:52:08 -0300

    AgregamosMetodoResta

--- GitHab/src/ProbamosGitHub.java
+++ GitHab/src/ProbamosGitHub.java
@@ -2,8 +2,11 @@
 public class ProbamosGitHub {
 
 	public static void main(String[] args) {
-
-		suma(2, 3);
+int a=3;
+int b=2;
+		suma(a, b);
+		resta(a, b);
+		
 		
 	}
 public static int suma(int a, int b) {
@@ -12,6 +15,12 @@ public static int suma(int a, int b) {
  System.out.println(rata);
 	return rata;
 	// TODO Auto-generated method stub
+
+}
+public static int resta (int a, int b) {
+	int rata = a-b;
+	System.out.println(rata);
+	return rata;
 
 }
 }
